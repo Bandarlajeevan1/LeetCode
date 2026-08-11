6e17c78f0d9a955952492b4082fb4e28d0486418
Jeevan Bandarla <jeevanbandarla1234@gmail.com>
2026-08-11 09:51:28 +0530

    Time: 27 ms (7.74%) | Memory: 81.2 MB (92.99%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        /*for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+            }
+        }
+    }
+    return false;*/
+    //return Arrays.stream(nums).distinct().count() !=nums.length;
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length-1;i++){
+        if(nums[i]==nums[i+1])
+            return true;
+    }
+    return false;
+    }
+}
